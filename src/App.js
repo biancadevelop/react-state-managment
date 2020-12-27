ff3ea9ac10bea12b7bf2a2f4121a94a72ae52d75
bianca Gonçalves <biancagoncalves@biancas-iMac.local>
2020-12-26 22:06:35 -0800

    added addmovie.js

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import React from 'react';
 import MovieList from './movielist';
 import Nav from './nav'
 import {MovieProvider} from './moviecontext'
+import AddMovie from './addmovie'
 
 function App() {
   return (
     <MovieProvider>
       <div className="App">
         <Nav/>
+        <AddMovie/>
         <MovieList/>
       </div>
     </MovieProvider>
